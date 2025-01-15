a1b36382757257814a163a7f2a950bd85bfb85c1
Bishoeri000 <Bishoeri000@warren.k12.in.us>
2025-01-15 12:23:38 -0500

    Second commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Happy developing ✨')
+const book = {
+    title: "The Adventures of Captain Underpants",
+    author: "Dav Pilkey",
+    year: 1997,
+    getSummery: function(){
+        return `${this.title} ${this.author} ${this.year};`
+    }
+};
+
+book.genre = "Adventure";
+delete book.year;
+console.log(book)
+
